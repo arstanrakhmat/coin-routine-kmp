d1b5fca43016d15d4e6e4419a6ef118bf6d585ff
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-06-30 15:59:04 +0600

    First commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,13 @@
+package org.example.project
+
+import androidx.compose.runtime.Composable
+import org.example.project.theme.CoinRoutineTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    CoinRoutineTheme {
+
+    }
+}
