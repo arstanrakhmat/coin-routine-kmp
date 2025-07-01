c3edb983f1f26030e20fb7f7210b82003ccd20b3
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-07-01 17:01:59 +0600

    Init koin in IOS and Android

--- composeApp/src/androidMain/kotlin/org/example/project/di/Module.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/di/Module.android.kt
@@ -2,7 +2,6 @@ package org.example.project.di
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.android.Android
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule = module {
